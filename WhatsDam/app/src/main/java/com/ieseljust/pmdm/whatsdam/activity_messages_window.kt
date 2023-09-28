bfb4f66dfc0d4e966869e23e959fced705f4a629
Pansero87 <prgsergiomb@gmail.com>
2023-09-28 08:46:38 +0200

    Aplicació cread para el mensaje

---
+++ WhatsDam/app/src/main/java/com/ieseljust/pmdm/whatsdam/activity_messages_window.kt
@@ -0,0 +1,11 @@
+package com.ieseljust.pmdm.whatsdam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_messages_window : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_messages_window)
+    }
+}
