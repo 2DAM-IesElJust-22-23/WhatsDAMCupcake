42bf05c8fee1e67a59b26305589bd95a5a6ed6f3
FernandoPereiraSalvador <fernandoplanet9000@gmail.com>
2023-09-28 09:28:01 +0200

    Ciclo de vida
    Se ha creado el ciclo de vida en el main.

--- WhatsDam/app/src/main/java/com/ieseljust/pmdm/whatsdam/MainActivity.kt
+++ WhatsDam/app/src/main/java/com/ieseljust/pmdm/whatsdam/MainActivity.kt
@@ -2,11 +2,64 @@ package com.ieseljust.pmdm.whatsdam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.ieseljust.pmdm.whatsdam.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Restaurar el valor del contador si existe un estado guardado
+        if (savedInstanceState != null) {
+
+        }
+    }
+    override fun onStart() {
+        super.onStart()
+        // Registro de log en onStart
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // Registro de log en onResume
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        // Registro de log en onPause
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        // Registro de log en onStop
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        // Registro de log en onRestart
+        Log.d(TAG, "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Registro de log en onDestroy
+        Log.d(TAG, "onDestroy")
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
     }
 }
