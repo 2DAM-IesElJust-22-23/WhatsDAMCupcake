b9e023a9a53097852def23a432b3354913563569
FernandoPereiraSalvador <fernandoplanet9000@gmail.com>
2023-09-30 18:08:48 +0200

    Eliminación código innecesario
    Se ha eliminado contenido innecesario

--- WhatsDam/app/src/main/java/com/ieseljust/pmdm/whatsdam/MainActivity.kt
+++ WhatsDam/app/src/main/java/com/ieseljust/pmdm/whatsdam/MainActivity.kt
@@ -5,7 +5,6 @@ import android.net.InetAddresses.isNumericAddress
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.annotation.RequiresApi
 import com.ieseljust.pmdm.whatsdam.databinding.ActivityMainBinding
 
@@ -21,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Restaurar el valor del contador si existe un estado guardado
+        // Restaurar el valor si existe un estado guardado
         if (savedInstanceState != null) {
             nickname = savedInstanceState.getString("nickname", "")
             ipserver = savedInstanceState.getString("ipserver", "")
@@ -35,7 +34,7 @@ class MainActivity : AppCompatActivity() {
 
             nickname = binding.nickNameText.text.toString()
             ipserver = binding.serverAddressText.text.toString()
-            
+
             // Iniciar la actividad de destino
             if((nickname != "") and isNumericAddress(ipserver)){
                 intent.putExtra("NICKNAME_KEY", nickname)
@@ -44,45 +43,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-    }
-    override fun onStart() {
-        super.onStart()
-        // Registro de log en onStart
-        Log.d(TAG, "onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        // Registro de log en onResume
-        Log.d(TAG, "onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        // Registro de log en onPause
-        Log.d(TAG, "onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        // Registro de log en onStop
-        Log.d(TAG, "onStop")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        // Registro de log en onRestart
-        Log.d(TAG, "onRestart")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        // Registro de log en onDestroy
-        Log.d(TAG, "onDestroy")
-    }
-
-    companion object {
-        private const val TAG = "MainActivity"
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
