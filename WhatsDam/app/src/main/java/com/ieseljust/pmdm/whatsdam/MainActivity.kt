27dd1b3d4e1cc1d8db2a996d212e6a7f302aeb62
FernandoPereiraSalvador <fernandoplanet9000@gmail.com>
2023-09-30 18:18:45 +0200

    Documentacion main
    Se ha documentado el main

--- WhatsDam/app/src/main/java/com/ieseljust/pmdm/whatsdam/MainActivity.kt
+++ WhatsDam/app/src/main/java/com/ieseljust/pmdm/whatsdam/MainActivity.kt
@@ -8,14 +8,20 @@ import android.os.Bundle
 import androidx.annotation.RequiresApi
 import com.ieseljust.pmdm.whatsdam.databinding.ActivityMainBinding
 
+/**
+ * Esta es la clase principal de la aplicación Android.
+ * Extiende de AppCompatActivity, que es una clase base para las actividades de Android.
+ */
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-    private var nickname=""
-    private var ipserver = ""
+    private lateinit var binding: ActivityMainBinding // Variable para vincular la vista XML a esta actividad
+    private var nickname="" // Almacena el nombre del usuario
+    private var ipserver = "" // Almacena la ip del servidor
+
     @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
 
+        // Inicializa la vista utilizando el enlace generado por ViewBinding.
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -26,16 +32,19 @@ class MainActivity : AppCompatActivity() {
             ipserver = savedInstanceState.getString("ipserver", "")
         }
 
+        // Obtiene una referencia al botón de conexión en la vista.
         val btConnect = binding.buttonConnect
 
+        // Configura una función para cuando se haga clic en btConnect
         btConnect.setOnClickListener {
             // Crear un Intent para ir a la actividad de destino
             val intent = Intent(this, Activity_messages_window::class.java)
 
+            // Obtiene los datos la vista.
             nickname = binding.nickNameText.text.toString()
             ipserver = binding.serverAddressText.text.toString()
 
-            // Iniciar la actividad de destino
+            // Iniciar la actividad de destino si el formato es correcto
             if((nickname != "") and isNumericAddress(ipserver)){
                 intent.putExtra("NICKNAME_KEY", nickname)
                 intent.putExtra("IPSERVER", ipserver)
@@ -45,12 +54,14 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    // Guarda el estado de la actividad
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putString("nickname", nickname)
         outState.putString("ipserver", ipserver)
     }
 
+    // Restaura el estado de la actividad
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         nickname = savedInstanceState.getString("nickname", "")
