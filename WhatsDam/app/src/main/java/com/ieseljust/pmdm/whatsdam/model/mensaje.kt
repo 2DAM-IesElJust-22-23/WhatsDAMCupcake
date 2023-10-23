104a390871cd51dc913dd2c15bcc5feb9e453d5f
FernandoPereiraSalvador <fernandoplanet9000@gmail.com>
2023-10-23 10:43:44 +0200

    Creacion del repositorio
    Se ha creado el repositorio de mensajes según el paso 1.3

--- WhatsDam/app/src/main/java/com/ieseljust/pmdm/whatsdam/model/mensaje.kt
+++ WhatsDam/app/src/main/java/com/ieseljust/pmdm/whatsdam/model/mensaje.kt
@@ -2,6 +2,3 @@ package com.ieseljust.pmdm.whatsdam.model
 
 // Clase que representa el mensaje, contendra el usuario que ha enviado el mensaje, el texto del mensaje y la hora del envío
 data class Mensaje(val usuario: String, val mensaje: String, val hora: String)
-
-// Lista que usaremos para almacenar los mensajes que enviamos
-val mensajesEnviados = mutableListOf<Mensaje>()
