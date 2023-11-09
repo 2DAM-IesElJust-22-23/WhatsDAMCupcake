a785eacb4dbb039efa9d3e77e35a38dcf5e634a2
FernandoPereiraSalvador <fernandoplanet9000@gmail.com>
2023-11-09 09:17:13 +0100

    Corrección del puerto

--- WhatsDAM_Java/client/src/main/java/com/ieseljust/psp/client/communications/serverListener.java
+++ WhatsDAM_Java/client/src/main/java/com/ieseljust/psp/client/communications/serverListener.java
@@ -29,7 +29,6 @@ public class serverListener implements Runnable {
      * li envia el servidor de missatgeria i ho processarà de forma adeqüada.
      * 
      */
-
     ViewModel vm;
 
     public serverListener(ViewModel vm) {
@@ -40,78 +39,82 @@ public class serverListener implements Runnable {
 
     @Override
     public void run() {
-        
+
         // TO-DO
         // 2. Iniciem un bucle infinit a l'espera de rebre connexions
         // Quan arribe una connexió la processrem de manera adeqüada
         // Les peticions que podme rebre seran de tipus:
-
         // {"type": "userlist", "content": [Llista d'usuaris]}, amb un JSONArray amb la llista d'usuaris.
         // {"type": "message", "user":"usuari", "content": "Contingut del missatge" }
-
         // És interessant implementar un mètode a banda per processat aquestes línies
         // però no cal que creem un fil a propòsit per atendre cada missatge, ja que
         // no som un servidor com a tal, i el que estem fent aci, és mantindre un 
         // canal de recepció només amb el servidor.
-    try {
+        ServerSocket serSocket = null;
+        try {
+            serSocket = new ServerSocket(0);
+            CurrentConfig.setlistenPort(serSocket.getLocalPort());
+        } catch (Exception e) {
 
-        while (true) {
-
-            ServerSocket serSocket = new ServerSocket(listenerPort);
-
-            // 2. Iniciamos un bucle infinito a la espera de recibir conexiones
-            Socket socket = serSocket.accept(); // Espera a que llegue una conexión
-
-            // Procesamos la conexión en un hilo aparte
-            new Thread(() -> {
-                try {
-                    // Aquí procesamos la entrada
-                    InputStream input = socket.getInputStream();
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-
-                    String messageString;
-                    StringBuilder sb = new StringBuilder();
-
-                    while ((messageString = reader.readLine()) != null) {
-                        sb.append(messageString);
-                    }
-
-                    // Una vez que hemos recibido el mensaje completo
-                    String receivedMessage = sb.toString();
-
-                    // Parseamos el mensaje como JSON
-                    JSONObject jsonMessage = new JSONObject(receivedMessage);
-
-                    // Identificamos el tipo de mensaje
-                    String messageType = jsonMessage.getString("type");
-
-                    if (messageType.equals("userlist")) {
-                        // Mensaje tipo lista de usuarios
-                        JSONArray userList = jsonMessage.getJSONArray("content");
-                        // Procesar la lista de usuarios
-                        // Hacer lo que sea necesario con la lista de usuarios
-                    } else if (messageType.equals("message")) {
-                        // Mensaje tipo mensaje de usuario
-                        String username = jsonMessage.getString("user");
-                        String content = jsonMessage.getString("content");
-                        // Procesar el mensaje del usuario
-                        // Hacer lo que sea necesario con el mensaje
-                    } else {
-                        System.out.println("Mensaje de tipo desconocido: " + messageType);
-                    }
-
-                    // Cerramos el socket después de procesar el mensaje
-                    socket.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }).start();
         }
+        try {
 
-    } catch (IOException e) {
-        System.out.println("Error al establecer la conexión: " + e.getMessage());
+            while (true) {
+
+                
+
+                // 2. Iniciamos un bucle infinito a la espera de recibir conexiones
+                Socket socket = serSocket.accept(); // Espera a que llegue una conexión
+
+                // Procesamos la conexión en un hilo aparte
+                new Thread(() -> {
+                    try {
+                        // Aquí procesamos la entrada
+                        InputStream input = socket.getInputStream();
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+                        String messageString;
+                        StringBuilder sb = new StringBuilder();
+
+                        while ((messageString = reader.readLine()) != null) {
+                            sb.append(messageString);
+                        }
+
+                        // Una vez que hemos recibido el mensaje completo
+                        String receivedMessage = sb.toString();
+
+                        // Parseamos el mensaje como JSON
+                        JSONObject jsonMessage = new JSONObject(receivedMessage);
+
+                        // Identificamos el tipo de mensaje
+                        String messageType = jsonMessage.getString("type");
+
+                        if (messageType.equals("userlist")) {
+                            // Mensaje tipo lista de usuarios
+                            JSONArray userList = jsonMessage.getJSONArray("content");
+                            // Procesar la lista de usuarios
+                            // Hacer lo que sea necesario con la lista de usuarios
+                        } else if (messageType.equals("message")) {
+                            // Mensaje tipo mensaje de usuario
+                            String username = jsonMessage.getString("user");
+                            String content = jsonMessage.getString("content");
+                            // Procesar el mensaje del usuario
+                            // Hacer lo que sea necesario con el mensaje
+                        } else {
+                            System.out.println("Mensaje de tipo desconocido: " + messageType);
+                        }
+
+                        // Cerramos el socket después de procesar el mensaje
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }).start();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error al establecer la conexión: " + e.getMessage());
+        }
     }
-}
-
 
 }
